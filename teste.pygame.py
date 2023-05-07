bdec6916467269d772727d6b3770422ece41e276
RyanDeivid <128193476+RyanDeivid@users.noreply.github.com>
2023-05-07 18:52:00 -0300

    Acrescentado uma tela de inicio.
    Blocos somem quando são atingidos, e pontuação onde cada bloco vale 10 pontos.

--- teste.pygame.py
+++ teste.pygame.py
@@ -2,6 +2,32 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+pygame.init()
+
+largura = 640
+altura = 480
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Breakout")
+relogio = pygame.time.Clock()
+
+# posição bola
+x_bola = 335
+y_bola = 460
+# posição prancha
+x_prancha = 300
+y_prancha = 467
+
+# Velocidade da bola
+velocidade_bola_x = 5
+velocidade_bola_y = -5
+
+bloco_largura = 62
+bloco_altura = 20
+padding_left = 16
+padding_top = 10
+linhas = 4
+colunas = 10
+bloco_lista = []
 
 def criar_blocos():
     for linha in range(linhas):
@@ -12,30 +38,45 @@ def criar_blocos():
             bloco = pygame.Rect(x, y, bloco_largura, bloco_altura)
             bloco_lista.append(bloco)
 
-
-pygame.init()
-
-largura = 640
-altura = 480
-"posição bola"
-x_bola = 335
-y_bola = 460
-"posição prancha"
-x_prancha = 300
-y_prancha = 467
-
-# blocos
-linhas = 4
-colunas = 4
-bloco_largura = 140
-bloco_altura = 40
-padding_left, padding_top = 10, 10
-bloco_lista = []
 criar_blocos()
 
-tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Breakout")
-relogio = pygame.time.Clock()
+pontuacao = 0
+pontuacao_fonte = pygame.font.Font(None, 36)  # Define a fonte para exibir a pontuação
+
+# Tela de início
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    # Desenhe a tela de início
+    tela.fill((0, 0, 0))
+    pygame.draw.rect(tela, (255, 255, 255), (100, 100, 440, 200), 2)
+    pygame.draw.rect(tela, (255, 255, 255), (120, 250, 200, 50))
+    pygame.draw.rect(tela, (255, 255, 255), (320, 250, 200, 50))
+    fonte_titulo = pygame.font.SysFont(None, 64)
+    texto_titulo = fonte_titulo.render("Breakout", True, (255, 0, 0))
+    tela.blit(texto_titulo, (180, 130))
+    fonte_opcoes = pygame.font.SysFont(None, 36)
+    texto_jogar = fonte_opcoes.render("Jogar", True, (255, 0, 0))
+    tela.blit(texto_jogar, (185, 260))
+    texto_sair = fonte_opcoes.render("Sair", True, (255, 255, 255))
+    tela.blit(texto_sair, (385, 260))
+    pygame.display.update()
+
+    # Verifique se o jogador selecionou uma opção
+    if pygame.mouse.get_pressed()[0]:
+        posicao_mouse = pygame.mouse.get_pos()
+        if posicao_mouse[0] >= 120 and posicao_mouse[0] <= 320 \
+                and posicao_mouse[1] >= 250 and posicao_mouse[1] <= 300:
+            # Inicie o jogo
+            break
+        elif posicao_mouse[0] >= 320 and posicao_mouse[0] <= 520 \
+                and posicao_mouse[1] >= 250 and posicao_mouse[1] <= 300:
+            # Saia do jogo
+            pygame.quit()
+            exit()
 
 
 while True:
@@ -46,16 +87,58 @@ while True:
             pygame.quit()
             exit()
 
+        # Verificar se a barra de espaço foi pressionada
+        if event.type == KEYDOWN and event.key == K_SPACE:
+            # Definir lancar_bola como True
+            lancar_bola = True
+            # Reiniciar a posição da bola
+            x_bola = 335
+            y_bola = 460
+
     if pygame.key.get_pressed()[K_a]:
         x_prancha = x_prancha - 10
     if pygame.key.get_pressed()[K_d]:
         x_prancha = x_prancha + 10
 
-    prancha = pygame.draw.rect(
-        tela, (255, 100, 100), (x_prancha, y_prancha, 70, 10))
-    bola = pygame.draw.circle(tela, (255, 255, 255), (x_bola, y_bola), 6)
+    # Atualiza a posição da bola
+    x_bola += velocidade_bola_x
+    y_bola += velocidade_bola_y
+
+    # Verifica se a bola atingiu a parede esquerda ou direita
+    if x_bola < 0 or x_bola > largura:
+        velocidade_bola_x = -velocidade_bola_x
+
+    # Verifica se a bola atingiu o teto
+    if y_bola < 0:
+        velocidade_bola_y = -velocidade_bola_y
+        y_bola = 0
 
     for bloco in bloco_lista:
-        pygame.draw.rect(tela, (214, 236, 192), bloco)
+        if bloco.colliderect(pygame.Rect(x_bola, y_bola, 5, 5)):
+            bloco_lista.remove(bloco)
+            velocidade_bola_y = -velocidade_bola_y
+            pontuacao += 10
+
+
+    # Verifica se a bola atingiu a prancha
+    if y_bola + 5 >= y_prancha and y_bola + 5 <= y_prancha + 10 and \
+            x_bola >= x_prancha and x_bola <= x_prancha + 70:
+        velocidade_bola_y = -velocidade_bola_y
+
+    # Atualiza a posição da prancha
+    prancha = pygame.Rect(x_prancha, y_prancha, 70, 10)
+
+    # Desenha todos os blocos na tela
+    for bloco in bloco_lista:
+        pygame.draw.rect(tela, (255, 255, 255,), bloco)
+
+    # Desenha a prancha e a bola na tela
+    bola = pygame.draw.circle(tela, (255, 255, 255), (x_bola, y_bola), 5)
+    pygame.draw.rect(tela, (255, 255, 255), prancha)
+
+    # Desenha a pontuação na tela
+    pontuacao_fonte = pygame.font.Font(None, 32)
+    pontuacao_texto = pontuacao_fonte.render(f"Pontuação: {pontuacao}", True, (255, 0, 0))
+    tela.blit(pontuacao_texto, (10, 10))
 
     pygame.display.update()
